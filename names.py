4590eb2efef0549433e8ab93a8dd87a75bf2a3cd
karkosz <ania.alp@vp.pl>
2024-11-10 15:05:20 +0100

    added age

--- names.py
+++ names.py
@@ -1,2 +1,5 @@
-print("Mam na imię...")  # tutaj wpisz swoje imię
+wiek = (0b100100)
+print("Mam na imię Anna i mam" , str(wiek) , "lat.")  # tutaj wpisz swoje imię
 # i dodaj jeszcze jakiś ciekawy kod
+
+
